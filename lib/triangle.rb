81a82cd6ebb638c6e81bbd2b0f71e435d87dedb4
Andrew Klein <andrew.klein.j@gmail.com>
2016-08-01 18:29:45 -0400

    complete assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
-class Triangle
-  # write code here
+class TriangleError < StandardError
+
 end
+
+
+class Triangle
+  
+
+  attr_accessor :length1, :length2, :length3, :kind, :lengths
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+    @lengths = [@length1, @length2, @length3]
+  end
+
+  def kind
+    if @lengths.all? {|length| length > 0} && !triangle_check
+      if @lengths.uniq.size == 1
+        :equilateral
+      elsif @lengths.uniq.size == 2
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+
+  end
+
+  def triangle_check
+    @length1 + @length2 <= @length3 || @length1 + @length3 <= @length2 || @length2 + @length3 <= @length1
+  end
+end
+
+
